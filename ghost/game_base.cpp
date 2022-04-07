6325f097c1b6576de07c86e3b7044761bbb5d661
Sam Leung <slyh@hk-fun.com>
2022-04-07 21:20:23 +0100

    disable some check

--- ghost/game_base.cpp
+++ ghost/game_base.cpp
@@ -1793,23 +1793,23 @@ void CBaseGame :: EventPlayerJoined( CPotentialPlayer *potential, CIncomingJoinP
 			JoinedRealm = (*i)->GetServer( );
 	}
 
-	if( JoinedRealm.empty( ) )
-	{
-		// the player is pretending to join via LAN, which they might or might not be (i.e. it could be spoofed)
-		// however, we've been broadcasting a random entry key to the LAN
-		// if the player is really on the LAN they'll know the entry key, otherwise they won't
-		// or they're very lucky since it's a 32 bit number
+	// if( JoinedRealm.empty( ) )
+	// {
+	// 	// the player is pretending to join via LAN, which they might or might not be (i.e. it could be spoofed)
+	// 	// however, we've been broadcasting a random entry key to the LAN
+	// 	// if the player is really on the LAN they'll know the entry key, otherwise they won't
+	// 	// or they're very lucky since it's a 32 bit number
 
-		if( joinPlayer->GetEntryKey( ) != m_EntryKey )
-		{
-			// oops!
+	// 	if( joinPlayer->GetEntryKey( ) != m_EntryKey )
+	// 	{
+	// 		// oops!
 
-			CONSOLE_Print( "[GAME: " + m_GameName + "] player [" + joinPlayer->GetName( ) + "|" + potential->GetExternalIPString( ) + "] is trying to join the game over LAN but used an incorrect entry key" );
-			potential->Send( m_Protocol->SEND_W3GS_REJECTJOIN( REJECTJOIN_WRONGPASSWORD ) );
-			potential->SetDeleteMe( true );
-			return;
-		}
-	}
+	// 		CONSOLE_Print( "[GAME: " + m_GameName + "] player [" + joinPlayer->GetName( ) + "|" + potential->GetExternalIPString( ) + "] is trying to join the game over LAN but used an incorrect entry key" );
+	// 		potential->Send( m_Protocol->SEND_W3GS_REJECTJOIN( REJECTJOIN_WRONGPASSWORD ) );
+	// 		potential->SetDeleteMe( true );
+	// 		return;
+	// 	}
+	// }
 
 	// check if the new player's name is banned but only if bot_banmethod is not 0
 	// this is because if bot_banmethod is 0 and we announce the ban here it's possible for the player to be rejected later because the game is full
@@ -2460,23 +2460,23 @@ void CBaseGame :: EventPlayerJoinedWithScore( CPotentialPlayer *potential, CInco
 			JoinedRealm = (*i)->GetServer( );
 	}
 
-	if( JoinedRealm.empty( ) )
-	{
-		// the player is pretending to join via LAN, which they might or might not be (i.e. it could be spoofed)
-		// however, we've been broadcasting a random entry key to the LAN
-		// if the player is really on the LAN they'll know the entry key, otherwise they won't
-		// or they're very lucky since it's a 32 bit number
+	// if( JoinedRealm.empty( ) )
+	// {
+	// 	// the player is pretending to join via LAN, which they might or might not be (i.e. it could be spoofed)
+	// 	// however, we've been broadcasting a random entry key to the LAN
+	// 	// if the player is really on the LAN they'll know the entry key, otherwise they won't
+	// 	// or they're very lucky since it's a 32 bit number
 
-		if( joinPlayer->GetEntryKey( ) != m_EntryKey )
-		{
-			// oops!
+	// 	if( joinPlayer->GetEntryKey( ) != m_EntryKey )
+	// 	{
+	// 		// oops!
 
-			CONSOLE_Print( "[GAME: " + m_GameName + "] player [" + joinPlayer->GetName( ) + "|" + potential->GetExternalIPString( ) + "] is trying to join the game over LAN but used an incorrect entry key" );
-			potential->Send( m_Protocol->SEND_W3GS_REJECTJOIN( REJECTJOIN_WRONGPASSWORD ) );
-			potential->SetDeleteMe( true );
-			return;
-		}
-	}
+	// 		CONSOLE_Print( "[GAME: " + m_GameName + "] player [" + joinPlayer->GetName( ) + "|" + potential->GetExternalIPString( ) + "] is trying to join the game over LAN but used an incorrect entry key" );
+	// 		potential->Send( m_Protocol->SEND_W3GS_REJECTJOIN( REJECTJOIN_WRONGPASSWORD ) );
+	// 		potential->SetDeleteMe( true );
+	// 		return;
+	// 	}
+	// }
 
 	// turning the CPotentialPlayer into a CGamePlayer is a bit of a pain because we have to be careful not to close the socket
 	// this problem is solved by setting the socket to NULL before deletion and handling the NULL case in the destructor
